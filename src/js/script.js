dd0f956d770e44e7cb596439d3d731c25c430489
Sionna Poleszuk <sionna.pol@gmail.com>
2021-10-13 15:10:42 +0200

    InitActions

--- src/js/script.js
+++ src/js/script.js
@@ -188,6 +188,9 @@
     constructor(element){
       const thisWidget = this;
       thisWidget.getElements(element);
+      thisWidget.initActions();
+      thisWidget.setValue(thisWidget.input.value);
+
       console.log('AmountWidget:',thisWidget);
       console.log('constructor arguments:', element);
     }
@@ -198,6 +201,33 @@
       thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
       thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
       thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+    setValue(value){
+      const thisWidget = this;
+      const newValue = parseInt(value);
+      /*TODO: Add validation*/
+      /* TODO: Add validation */
+      if(thisWidget.value !== newValue && !isNaN(newValue)) {
+        thisWidget.value = newValue;
+      }
+      thisWidget.value = newValue;
+      thisWidget.input.value = thisWidget.value;
+    }
+    initActions(){
+      const thisWidget = this;
+      thisWidget.input.addEventListener('change', function(){
+        thisWidget.setValue(thisWidget.input);
+      });
+      thisWidget.linkDecrease.addEventListener('click', function(event){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value-1);
+      });
+      thisWidget.linkIncrease.addEventListener('click',function(event){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value+1);
+      });
+
+
     }
   }
   const app = {
