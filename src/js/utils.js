9b4369830dd7e378df13646d9cdba36b5877acc2
Sionna Poleszuk <sionna.pol@gmail.com>
2021-11-03 15:46:47 +0100

    10.3 components-doesn't work

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,6 +1,6 @@
 /* global Handlebars, dataSource */
-
-const utils = {}; // eslint-disable-line no-unused-vars
+import app from './app.js';
+export const utils = {}; // eslint-disable-line no-unused-vars
 
 utils.createDOMFromHTML = function(htmlString) {
   let div = document.createElement('div');
